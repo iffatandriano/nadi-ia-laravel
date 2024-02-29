8ccd516c9ffe563d639c8231b28a65f3a87894ae
Iffat Andriano <iffatandriano@IA-3.local>
2024-02-29 17:29:19 +0700

    add: layout login page and implements login, logout

---
+++ resources/js/Layouts/ContainerLayout.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ContainerLayout({ children }) {
+    return <div className="max-w-[2520px] mx-auto">{children}</div>;
+}
--- routes/web.php
+++ routes/web.php
@@ -16,23 +16,8 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/dashboard', function () {
+Route::get('/', function() {
     return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+})->middleware(['auth', 'verified'])->name('home');
 
 require __DIR__.'/auth.php';
